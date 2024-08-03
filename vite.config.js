3028980528605095caa6412c87a8ab0e645795e7
waahhoo <alextirador512@gmail.com>
2024-08-03 00:08:41 -0400

    fixed page name and icon

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/alex-tirador/",
   plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+    },
+  },
 })
