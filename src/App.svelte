40ac9267f0481b6f0e28b67a40c13698d94ca28b
waahhoo <alextirador512@gmail.com>
2024-08-10 12:12:55 -0400

    fixed tailwind, added shacdn, changed a bit of the website

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,7 @@
 <script>
+import "./app.css";
+import * as Avatar from "$lib/components/ui/avatar";
+import * as HoverCard from "$lib/components/ui/hover-card";
 import camera from '../images/camera.png'
 import coffee from '../images/coffee.jpg'
 import homePage from '../images/homePage.png'
@@ -7,27 +10,28 @@ import snow from '../images/snow.jpg'
 import waahhoo from '../images/waahhoo.png'
 </script>
 
-<div class="bg-center bg-cover ..." style="background-image:var(homePage)">
-	<img src="{homePage}" alt="d">
+<div class="flex flex-col h-screen items-center justify-center">
+	<h1 class="text-6xl">Waahhoo</h1>
+	<HoverCard.Root>
+		<HoverCard.Trigger>
+			<Avatar.Root class="h-48 w-48">
+				<Avatar.Image src="{waahhoo}" alt="avatar"/>
+				<Avatar.Fallback>Waahhoo</Avatar.Fallback>
+			</Avatar.Root>
+		</HoverCard.Trigger>
+		<HoverCard.Content class="w-80">
+			<div class="flex justify-between">
+				<Avatar.Root class="h-16 w-16">
+					<Avatar.Image src="{waahhoo}" alt="avatar"/>
+					<Avatar.Fallback>Waahhoo</Avatar.Fallback>
+				</Avatar.Root>
+				<div class="space-y-1">
+					<h4 class="text-md font-semibold">Alex Tirador</h4>
+					<p class="text-sm">Junior CS Major @</p>
+					<p class="text-sm">The University of Maryland</p>
+				</div>
+			</div>
+		</HoverCard.Content>
+	</HoverCard.Root>
+	<p class="text-lg">Hey look this works!</p>
 </div>
-
-<style>
-/* .logo {
-	height: 10em;
-	padding: 1.5em;
-	padding-left: 22%;
-	will-change: filter;
-	transition: filter 400ms;
-}
-.logo:hover {
-	filter: drop-shadow(0 0 2em #646cffaa);
-} */
-
-	/* .hi{
-		position: fixed;
-		top: 0%;
-		left: 0%;
-		max-width:screen;
-		max-height: screen;
-	} */
-</style>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,11 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: 'index.html',
+    },
+  },
+  resolve: {
+    alias: {
+      $lib: path.resolve("./src/lib"),
     },
   },
 })
